7e8fe2fd1ad90551476ed71d676443ece5822c6a
keopscakmak2121 <keopscakmak2121@gmail.com>
2026-02-15 11:07:13 +0300

    Otomatik commit

--- git_gonder.bat
+++ git_gonder.bat
@@ -11,11 +11,14 @@ if "%message%"=="" set message=Otomatik commit
 
 git commit -m "%message%"
 
-REM Push yap
-git push origin master:main
+REM Hangi branch'taysak onu push et
+for /f "tokens=*" %%a in ('git branch --show-current') do set BRANCH=%%a
+echo Aktif branch: %BRANCH%
+
+git push origin %BRANCH%:main
 if errorlevel 1 (
     echo Push basarisiz, force push deneniyor...
-    git push -f origin master:main
+    git push -f origin %BRANCH%:main
 )
 
 echo Islem tamamlandi!
